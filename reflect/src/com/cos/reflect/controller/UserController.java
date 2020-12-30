d5deda8d00a4ebffbc2bef4a176fd8a04587f20d
kutaemo <ktm6044@daum.net>
2020-12-30 17:29:38 +0900

    리플렉션

--- reflect/src/com/cos/reflect/controller/UserController.java
+++ reflect/src/com/cos/reflect/controller/UserController.java
@@ -1,16 +1,42 @@
 package com.cos.reflect.controller;
 
+import com.cos.reflect.anno.RequestMapping;
+import com.cos.reflect.controller.dto.JoinDto;
+import com.cos.reflect.controller.dto.LoginDto;
+import com.cos.reflect.model.User;
+
 public class UserController {
 
-	public void join() {
-		System.out.println("join() 함수 호출됨");
-	}
-	
-	public void login() {
-		System.out.println("login() 함수 호출됨");
-	}
-	
-	public void user() {
-		System.out.println("user() 함수 호출됨");
-	}
+		@RequestMapping("/user/join")
+		public String join(JoinDto dto) {
+			System.out.println("join() 함수 호출됨");
+			System.out.println(dto);
+			return "/"; 	// /join 대신에 /가 들어가서 index.jsp 실행됨
+		}
+
+		@RequestMapping("/user/login")
+		public String login(LoginDto dto) {
+			System.out.println("login() 함수 호출됨");
+			System.out.println(dto);
+			return "/";
+		}
+
+		@RequestMapping("/user/list")
+		public String list(User user) { // id, username, password, email
+			System.out.println("list() 함수 호출됨");
+			System.out.println(user);
+			return "/";
+		}
+		
+		@RequestMapping("/user")
+		public String user() {
+			System.out.println("user() 함수 호출됨");
+			return "/";
+		}
+
+		@RequestMapping("/hello")
+		public String hello() {
+			System.out.println("hello() 함수 호출됨");
+			return "/";
+		}
 }
